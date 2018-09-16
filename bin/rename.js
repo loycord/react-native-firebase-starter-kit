9a0d4fd0c1e68cfe6ced84e5d98282b643339dbf
Seungyeon Kim <loycord.ceo@gmail.com>
2018-09-17 00:35:21 +0900

    Fixed rename default value

--- bin/rename.js
+++ bin/rename.js
@@ -3,9 +3,9 @@ const readline = require('readline');
 const replace = require('replace-in-file');
 
 const BASE_DIRECTORY = './';
-const DEFAULT_COMPANY_NAME = 'invertase';
-const DEFAULT_PACKAGE_NAME = 'com.invertase.rnfirebasestarter';
-const DEFAULT_PROJECT_NAME = 'RNFirebaseStarter';
+const DEFAULT_COMPANY_NAME = 'midnightplan';
+const DEFAULT_PACKAGE_NAME = 'com.midnightplan.rnfirebasestarterkit';
+const DEFAULT_PROJECT_NAME = 'RNFirebaseStarterKit';
 const VALID_CHARACTERS = /^[a-zA-Z\s]+$/;
 
 const rl = readline.createInterface({
